2b12bb729a59f1b371f03687b4eb5477566692ad
José Antonio Álvarez <ocetebt@hotmail.com>
2019-01-26 10:57:44 +0100

    Task and TaskManager finished (kinda)

--- GlobalGameJam2019/Assets/Scripts/TaskManager.cs
+++ GlobalGameJam2019/Assets/Scripts/TaskManager.cs
@@ -6,6 +6,9 @@ public class TaskManager : MonoBehaviour {
     
     private float timeBetweenTasks;
     private float currentTime;
+    private int numTasks;
+    private int maxActiveTasks;
+    private Random random;
 
     // Asignar externamente
     public List<Task> tasks;
@@ -17,23 +20,40 @@ public class TaskManager : MonoBehaviour {
             t.Create( player1, player2, this );
         }
 
+        random = new Random();
+
         timeBetweenTasks = 5;
-        currentTime = 0;
+        currentTime = 1;
+
+        maxActiveTasks = 6;
+        numTasks = 0;
     }
 
     // Update is called once per frame
     void Update() {
 
         currentTime -= Time.deltaTime;
-        
+
+        if (numTasks >= maxActiveTasks) {
+            currentTime = timeBetweenTasks;
+        }
+
         if (currentTime <= 0) {
-
-
-
-
+            ActivateTask();
             currentTime = timeBetweenTasks;
         }
     }
 
+    void ActivateTask() {
+        bool activated = false;
+        while (!activated) {
+            int randomNumber = random.Next(0, tasks.Count() );
+            activated = tasks[randomNumber].Initialize();
+        }
+        numTasks++;
+    }
 
+    public void TaskCompleted() {
+        numTasks--;
+    }
 }
