7f455bb99507bb87042ee9b059d2cbb035c19368
José Antonio Álvarez <ocetebt@hotmail.com>
2019-01-26 08:54:38 +0100

    Pushing Player

---
+++ GlobalGameJam19/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private Animator animator;
+    public LayerMask blockingLayer;
+    private AudioSource audioSource;
+
+    public int stress;
+
+    // Start is called before the first frame update
+    void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1.0f / moveTime;
+        animator = GetComponent<Animator>();
+        activated = true;
+        death = false;
+
+        stress = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+
+
+
+    }
+}
