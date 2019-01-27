5336b0d14bbe5b097e2f4ab308526c1ba33437ad
Luis Sanchez <hfegetude@gmail.com>
2019-01-27 11:37:01 +0100

    Fixed Audio looping bug (not really but who cares)

--- GlobalGameJam2019/Assets/Scripts/AudioManager/AudioManager.cs
+++ GlobalGameJam2019/Assets/Scripts/AudioManager/AudioManager.cs
@@ -11,10 +11,14 @@ public class AudioManager : MonoBehaviour {
     private Dictionary<string, AudioNode> nodeDict = new Dictionary<string, AudioNode>();
     [SerializeField]
     private List<AudioClip>clips;
+    [SerializeField]
+    private PlayerGameController playerGameController;
+
+    private AudioNode currentNode;
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
-
+        
         nodeDict["START"] = new AudioNode(new Vector2(0, 0), null, null, clips[0]);
         nodeDict["LowStressLoop"] = new AudioNode(new Vector2(0, 33), null, null, clips[1]);
         nodeDict["MediumStressStart"] = new AudioNode(new Vector2(33, 66), null, null, clips[2]);
@@ -30,22 +34,33 @@ public class AudioManager : MonoBehaviour {
         nodeDict["LowStressLoop"].setDefaultNode(nodeDict["MediumStressStart"]);
         nodeDict["MediumStressStart"].setReachableNodes(new List<AudioNode> { nodeDict["LowStressLoop"], nodeDict["MediumStressLoop"] });
         nodeDict["MediumStressStart"].setDefaultNode(nodeDict["MediumStressLoop"]);
-        nodeDict["MediumStressLoop"].setReachableNodes(new List<AudioNode> { nodeDict["MediumStressLoop"], nodeDict["MediumStressStart"], nodeDict["MediumStressExit"] });
+        nodeDict["MediumStressLoop"].setReachableNodes(new List<AudioNode> { nodeDict["MediumStressLoop"], nodeDict["LowStressLoop"], nodeDict["MediumStressExit"] });
         nodeDict["MediumStressLoop"].setDefaultNode(nodeDict["MediumStressExit"]);
-        nodeDict["MediumStressExit"].setReachableNodes(new List<AudioNode> { nodeDict["MediumStressLoop"], nodeDict["HighStressStart"] });
-        nodeDict["MediumStressExit"].setDefaultNode(nodeDict["MediumStressLoop"]);
-        nodeDict["HighStressStart"].setReachableNodes(new List<AudioNode> {nodeDict["MediumStressExit"], nodeDict["HighStressLoop"] });
-        nodeDict["HighStressStart"].setDefaultNode(nodeDict["HighStressExit"]);
-        nodeDict["HighStressLoop"].setReachableNodes(new List<AudioNode> { nodeDict["HighStressLoop"], nodeDict["HighStressExit"] });
+        nodeDict["MediumStressExit"].setReachableNodes(new List<AudioNode> {  nodeDict["HighStressStart"] });
+        nodeDict["MediumStressExit"].setDefaultNode(nodeDict["HighStressStart"]);
+        nodeDict["HighStressStart"].setReachableNodes(new List<AudioNode> {nodeDict["MediumStressLoop"], nodeDict["HighStressLoop"] });
+        nodeDict["HighStressStart"].setDefaultNode(nodeDict["HighStressLoop"]);
+        nodeDict["HighStressLoop"].setReachableNodes(new List<AudioNode> { nodeDict["HighStressLoop"], nodeDict["MediumStressLoop"] });
         nodeDict["HighStressLoop"].setDefaultNode(nodeDict["HighStressExit"]);
-        nodeDict["HighStressExit"].setReachableNodes(new List<AudioNode>{nodeDict["MediumStressLoop"] });
-        nodeDict["HighStressExit"].setDefaultNode(nodeDict["MediumStressLoop"]);
+        
 
+        currentNode = nodeDict["START"];
+        audioSource.clip = currentNode.getAudioClip();
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update () {
+        float totalstress = playerGameController.getMaxStressInPlayers();
+        AudioNode nextAudioNode = currentNode.nextChild(totalstress);
+        Debug.Log(audioSource.clip.name);
+        if (currentNode.getAudioClip().name != nextAudioNode.getAudioClip().name){
+            audioSource.loop = false;
+        }
         if (!audioSource.isPlaying){
+            currentNode = nextAudioNode;
+            audioSource.clip = currentNode.getAudioClip();
+            audioSource.loop = true;
             audioSource.Play();
         }
     }
