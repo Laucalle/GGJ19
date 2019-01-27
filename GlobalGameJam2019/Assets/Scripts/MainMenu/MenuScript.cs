b609b14ccbf3487fee89ebdda2c4773cbdb7ea30
Javier León Palomares <jleon95@users.noreply.github.com>
2019-01-27 13:59:09 +0100

    Created menu draft

---
+++ GlobalGameJam2019/Assets/Scripts/MainMenu/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+	
+	public void LoadSceneAsync(string sceneName){
+
+		StartCoroutine(LoadYourAsyncScene(sceneName));
+	}
+
+	IEnumerator LoadYourAsyncScene(string sceneName){
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync (sceneName);
+
+		while (!asyncLoad.isDone)
+			yield return null;
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+}
